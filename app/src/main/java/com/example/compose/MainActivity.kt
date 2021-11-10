e36033ed3b8457fc5b40811fc4c4b00ea1355a1d
tugceerdogan <tugce.0483@gmail.com>
2021-11-10 18:12:56 +0300

    Display image and multiple text

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,45 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType.Companion.Em
+import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
-import com.example.compose.ui.theme.Teal200
+import com.example.compose.ui.theme.Purple700
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard(message = Message("Zela","Hi, how are you?"))
                 }
             }
         }
     }
 }
+data class Message(val author: String, val body : String)
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!",
-        color= Teal200,
-        fontStyle = FontStyle.Italic,
-        fontFamily = FontFamily.Monospace)
+fun MessageCard(message: Message){
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_pic),
+            contentDescription = "Contact pp",
+            modifier = Modifier
+                .size(30.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+
+        Column {
+            Text(text = message.author, color= Purple700, fontFamily = FontFamily.Monospace)
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(text = message.body)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewMessageCard() {
     ComposeTheme {
-        Greeting("Android")
+        MessageCard(message = Message("Zela","Hi, how are you?"))
     }
 }
