02b81b638fbb33f40bc46a9a1af5e674dc90cb88
tugceerdogan <tugce.0483@gmail.com>
2021-11-22 17:19:10 +0300

    Add list and animations

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,19 +4,26 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import com.example.compose.ui.SampleData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +31,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    MessageCard(message = Message("Zela","Hi, how are you?"))
+                    //MessageCard(message = Message("Zela","Hi, how are you?"))
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -36,15 +44,21 @@ data class Message(val author: String, val body : String)
 fun MessageCard(message: Message){
     Row (modifier = Modifier.padding(all = 10.dp)) {
         Image(
-            painter = painterResource(id = R.drawable.profile_pic),
+            painter = painterResource(id = R.drawable.profile_picc),
             contentDescription = "Contact pp",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
         )
-        Spacer(modifier = Modifier.width(5.dp))
 
-        Column {
+        Spacer(modifier = Modifier.width(5.dp))
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+
             Text(
                 text = message.author,
                 color= MaterialTheme.colors.secondaryVariant,
@@ -53,28 +67,43 @@ fun MessageCard(message: Message){
 
             Spacer(modifier = Modifier.height(3.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 2.dp){
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 2.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ){
                 Text(
                     text = message.body,
                     modifier = Modifier.padding(all = 6.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
-
         }
     }
 }
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
 @Preview(name = "Light Mode")
+
 @Preview(
     name = "Dark Mode",
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
 )
+
 @Composable
-fun PreviewMessageCard() {
+fun PreviewConversation() {
     ComposeTheme {
-        MessageCard(
-            message = Message("Zela","Hi, how are you?")
-        )
+        Conversation(SampleData.conversationSample)
     }
 }
---
+++ app/src/main/java/com/example/compose/ui/SampleData.kt
@@ -0,0 +1,66 @@
+package com.example.compose.ui
+
+import com.example.compose.Message
+
+object SampleData {
+    val conversationSample = listOf(
+        Message(
+            "Zela",
+            "Hello!"
+        ),
+        Message(
+            "Andrew",
+            "Hi, how're you :)"
+        ),
+        Message(
+            "Zela",
+            "I'm fine I'm watching a TV show\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "Andrew",
+            "Great, I was wondering what are you watching? :)"
+        ),
+        Message(
+            "Zela",
+            "Office!\n" +
+                    "I think it's the funniest show in the world."
+        ),
+        Message(
+            "Andrew",
+            "Really, I didn't watch that show :("
+        ),
+        Message(
+            "Zela",
+            "What a shame! You must watch now :) "
+        ),
+        Message(
+            "Andrew",
+            "How many seasons ?"
+        ),
+        Message(
+            "Zela",
+            "9 ^_^"
+        ),
+        Message(
+            "Andrew",
+            "Really! I don't have that much time :("
+        ),
+        Message(
+            "Zela",
+            "I think you can watch it in your spare time "
+        ),
+        Message(
+            "Andrew",
+            "You're right, i'll definitely look :)"
+        ),
+        Message(
+            "Zela",
+            "Ok, have a good day buddy :)"
+        ),
+        Message(
+            "Andrew",
+            "Bye :)"
+        ),
+    )
+}
